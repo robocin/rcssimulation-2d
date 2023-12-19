29ae8af9caa9e5411edc5cc0a74af9e82996f3fb
mateusfbsoares <mateusfbsoares@gmail.com>
2023-12-19 10:54:55 -0300

    add simple_client executable

---
+++ rcssimulation/simple_client.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Mateus Ferreira <mateusfbsoares> on 12/19/23.
+// Copyright (c) 2023 RobôCIn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello Simple Client" << std::endl;
+}
